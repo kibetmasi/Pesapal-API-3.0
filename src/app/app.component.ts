a4d1266d326aeb47de9bbfcc53f6058b10fbcfc6
kibet masiror <32261713+MicroTot@users.noreply.github.com>
2022-09-08 16:47:22 +0300

    id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,33 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-const demoUrl = 'https://cybqa.pesapal.com/pesapalv3'
+// const demoUrl = 'https://cybqa.pesapal.com/pesapalv3'
+const demoUrl = '	https://pay.pesapal.com/v3'
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
+  <div class="text">
+  <a href="https://documenter.getpostman.com/view/6715320/UyxepTv1">API reference</a>
+  </div>
     <div class="fullscreen-container">
       <div class="login-container">
-        <h1>Pesapal API</h1>
+        <h1>Pesapal API 3.0</h1>
           <form class="form" [formGroup]="form">
             <div class="input-group">
+            <mat-form-field>
+              <mat-label>Currency</mat-label>
+              <mat-select formControlName="currency">
+                <mat-option *ngFor="let cash of currencies" value="{{cash.value}}">{{cash.name}}</mat-option>
+              </mat-select>
+            </mat-form-field>
               <input formControlName="amount" type="number" placeholder="Amount" id="text" name="text" class="input-username"> 
             </div>        
             <div class="input-group">
               <textarea formControlName="description" placeholder="Description" id="password" name="password" class="input-password"></textarea>
             </div>      
-            <input placeholder="Make payment" class="button" [disabled]="!form.valid" (click)="getToken()">    
+            <input type="submit" placeholder="Make payment" class="button" [disabled]="!form.valid" (click)="getToken()">    
           </form>    
     </div>
   </div>
@@ -27,7 +37,7 @@ const demoUrl = 'https://cybqa.pesapal.com/pesapalv3'
 })
 export class AppComponent implements OnInit {
   form!:FormGroup
-  demoKeys:any = { 
+  demoKeys:any = {  //demo keys
     "consumer_key": "vssEbl/R5gzC/Lqu29nKF+UEMa0ppDzz",
     "consumer_secret": "fKbaakP8ZyRZUh+vbmvLlOxUHOs=" 
   }
@@ -35,7 +45,14 @@ export class AppComponent implements OnInit {
     "url": "http://www.example.com",
     "ipn_notification_type": "GET"
   }
-  token:any;
+  currencies:any = [
+    { "name":"Kenyan Shilling",  "value":"KES" },
+    { "name":"US Dollar", "value":"USD" },
+    { "name":"Euro", "value":"EUR" },
+    { "name":"Great Britain Pound", "value":"GBP" },
+    { "name":"Ugandan Shilling", "value":"UGX" },
+    { "name":"Tanzanian Shilling", "value":"TZX" },
+  ]
   iframe:any;
   constructor(
     private http: HttpClient, 
@@ -44,7 +61,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      currency: ['KES', [Validators.required]],
+      currency: ['', [Validators.required]],
       amount: ['', [Validators.required]],
       description: ['', [Validators.required, Validators.maxLength(100)]]
     })
@@ -54,18 +71,16 @@ export class AppComponent implements OnInit {
     this.http.post(`${demoUrl}/api/Auth/RequestToken`, this.demoKeys) //generate access token
     .subscribe({
       next: (res:any) => {
-        this.token = res.token
         const headers = new HttpHeaders()
-        .set('Authorization', `Bearer ${this.token}`)
+        .set('Authorization', `Bearer ${res.token}`)
         this.http.post(`${demoUrl}/api/URLSetup/RegisterIPN`, this.regIpn, {headers: headers}).subscribe({ //get IPN
           next: (res:any) => {
-            const order:any = 
-            {
-              "id": "TEST1515111110",
+            const order:any = {
+              "id": Date.now().toString(36) + Math.random().toString(36).substring(2),
               "currency": this.form.getRawValue().currency,
               "amount": this.form.getRawValue().amount,
               "description": this.form.getRawValue().description,
-              "callback_url": "https://www.example.com/payment-confirmed",
+              "callback_url": "http://localhost:4200",
               "notification_id": res.ipn_id,
               "billing_address": {
                   "email_address": "john.doe@example.com",
@@ -85,7 +100,8 @@ export class AppComponent implements OnInit {
             .subscribe({
               next: (res:any) => {
                 this.iframe = res.redirect_url //load payment iframe
-              }
+              },
+              error: (error:any) => console.error(error)
             })
           }
         })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IframePipe } from './iframe.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -18,6 +22,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
